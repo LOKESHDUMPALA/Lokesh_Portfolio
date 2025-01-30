4f4152adabeb720e3bd553482a1f765bdbc47b04
LOKESH DUMPALA <133016649+LOKESHDUMPALA@users.noreply.github.com>
2025-01-30 13:16:01 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -21,7 +21,7 @@ export const Bio = {
       "I am a motivated and versatile individual, always eager to take on new challenges.",
     github: "https://github.com/LOKESHDUMPALA",
     resume:
-      " https://drive.google.com/file/d/1c8LVQD4DqJl7TCvYGgiWNYuWoqtjtcSB/view?usp=sharing",
+      "https://drive.google.com/file/d/1gvFSGVrNoBKfLVXCa6x81zxr-njp_hBF/view?usp=sharing",
     linkedin: "https://www.linkedin.com/in/lokeswara-rao-dumpala-31708925a/",
     twitter: "https://x.com/DumpalaLok18739?t=YitygsW-moySAotMsFFvxg&s=08",
     insta: "https://www.instagram.com/dumpalalokesh/profilecard/?igsh=MW9sMTZvdDBoZnhrYQ==",
@@ -38,6 +38,12 @@ export const Bio = {
           image:
             "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
         },
+          {
+          name: "Next Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+          
         {
           name: "Redux",
           image:
@@ -335,8 +341,8 @@ export const Bio = {
         "Context-API",
         "Bcrypt"
       ],
-      github: "https://github.com/sharmi4590/domana",
-      webapp: "https://domain-analyzer-ten.vercel.app/",
+      github: "https://github.com/LOKESHDUMPALA/TEAM_INFO",
+      webapp: "https://team-info-gamma.vercel.app/",
     },
     {
       id: 3,
